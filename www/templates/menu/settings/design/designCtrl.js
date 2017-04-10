5da63745e2de2922e421857ab4592e282af90d43
Bhavin <bhavin.p_productivet@outlook.com>
2017-04-10 08:49:49 +0530

    bhavin_design

--- www/templates/menu/settings/design/designCtrl.js
+++ www/templates/menu/settings/design/designCtrl.js
@@ -1,31 +1,6 @@
 ﻿angular.module('starter.design', [])
     .controller('DesignCtrl', function ($scope, $state, $ionicModal, $stateParams, $timeout, Restangular) {
-
         var vm = this;
-        $ionicModal.fromTemplateUrl('#/app/design', {
-            scope: $scope,
-            animation: 'slide-in-up'
-        }).then(function (modal) {
-            $scope.modal = modal;
-        });
-        $scope.openModal = function () {
-            $scope.modal.show();
-        };
-        $scope.closeModal = function () {
-            $scope.modal.hide();
-        };
-        // Cleanup the modal when we're done with it!
-        $scope.$on('$destroy', function () {
-            $scope.modal.remove();
-        });
-        // Execute action on hide modal
-        $scope.$on('modal.hidden', function () {
-            // Execute action
-        });
-        // Execute action on remove modal
-        $scope.$on('modal.removed', function () {
-            // Execute action
-        });
         vm.list = [];
         vm.save = save;
         vm.edit = edit;
